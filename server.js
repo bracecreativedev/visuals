a126131de423abbae6e4d599b28dfe5c5edd8467
mattdobsonweb <mpd.mattdobson@icloud.com>
2018-10-08 09:40:53 +0100

    Change of port

--- server.js
+++ server.js
@@ -29,4 +29,4 @@ if (process.env.NODE_ENV === "production") {
 const port = process.env.PORT || 5000;
 
 //
-app.listen(5000, () => console.log("App listening on 5000"));
+app.listen(port, () => console.log("App listening on 5000"));
